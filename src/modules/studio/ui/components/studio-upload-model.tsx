ce5e2b83a954a462b730a18c82d3836efa893d57
ASGAIKMM <123511666+ASGAIKMM@users.noreply.github.com>
2025-02-18 16:06:24 +0200

    mux integration

--- src/modules/studio/ui/components/studio-upload-model.tsx
+++ src/modules/studio/ui/components/studio-upload-model.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import { ResponsizeDialog } from "@/components/responsize-dialog";
+import { StudioUploader } from "./studio-uploader";
 
 export function StudioUploadModel() {
   const utils = trpc.useUtils();
@@ -16,18 +18,31 @@ export function StudioUploadModel() {
     },
   });
   return (
-    <Button
-      variant="secondary"
-      onClick={() => create.mutate()}
-      disabled={create.isPending}
-      className="cursor-pointer"
-    >
-      {create.isPending ? (
-        <Loader2Icon className="size-4 mr-2 animate-spin" />
-      ) : (
-        <PlusIcon className="size-4 mr-2" />
-      )}
-      {create.isPending ? "Creating..." : "Create"}
-    </Button>
+    <>
+      <ResponsizeDialog
+        title="Upload a Video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon className="animate-spin" />
+        )}
+      </ResponsizeDialog>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+        className="cursor-pointer"
+      >
+        {create.isPending ? (
+          <Loader2Icon className="size-4 mr-2 animate-spin" />
+        ) : (
+          <PlusIcon className="size-4 mr-2" />
+        )}
+        {create.isPending ? "Creating..." : "Create"}
+      </Button>
+    </>
   );
 }
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { videos } from "@/db/schema";
 import { db } from "@/db";
+import { mux } from "@/lib/mux";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
@@ -8,13 +9,23 @@ export const videosRouter = createTRPCRouter({
       user: { id: userId },
     } = ctx;
 
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+      },
+      cors_origin: "*", // TODO: in production, set to url
+    });
+
     const [video] = await db
       .insert(videos)
       .values({
         userId,
         title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
-    return { video };
+    return { video, url: upload.url };
   }),
 });
