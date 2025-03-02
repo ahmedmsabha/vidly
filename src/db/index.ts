7f94cb7e28e2e22eca6e4e7b6fabdb67e57e9ff3
ASGAIKMM <123511666+ASGAIKMM@users.noreply.github.com>
2025-03-02 16:49:23 +0200

    fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
 config({ path: ".env.local" });
+config({ path: ".env" });
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
